6f77feb9698c32a0eb26aa01236885ab5a2de926
Davin-Liang <1528994924@qq.com>
2025-10-14 18:00:33 +0800

    正在编写上位机和下位机的通讯接口

--- Software/STM32_MW-Charging-Aux-Board/User/App/Src/dm542_task.c
+++ Software/STM32_MW-Charging-Aux-Board/User/App/Src/dm542_task.c
@@ -6,11 +6,14 @@
 #include "command.h"
 #include "command_task.h"
 #include "data_sum_task.h"
+#include "lwip_recv_task.h"
+#include "command_struct.h"
 
 static void dm542_Task(void * param); /* Test_Task任务实现 */
 
 static TaskHandle_t g_dm542TaskHandle = NULL;/* LED任务句柄 */
 static struct CommandInfo * command;
+static MotorCmd_t motorCmd;
 
 extern SemaphoreHandle_t dm542_USART3_Mutex;
 
@@ -25,32 +28,40 @@ static void dm542_Task(void * param)
 	insert_task_handle(g_dm542TaskHandle, "dm542");
   struct MotorData_t currentMotorData;
   
-    while (1)
+  while (1)
   {
     // vTaskDelay(5000);
     switch ((int)command->commandType)
     {
         case demandTwo:
-            for (float x = -30.0f; x < 30.0f; x = x + 10)
+          for (float x = -30.0f; x < 30.0f; x = x + 10)
+          {
+            for (float y = -30.0f; y < 30.0f; y = y + 10)
             {
-                for (float y = -30.0f; y < 30.0f; y = y + 10)
-                {
-                    xSemaphoreTake(dm542_USART3_Mutex, portMAX_DELAY);
-                    motor_position_ctrl(x, y);
-                    currentMotorData.x = x;
-                    currentMotorData.y = y;
-                    xQueueSend(g_motorDataQueue, &currentMotorData, 10);
-                    //mutual_printf("Position:(%.2f,%.2f)\r\n", x, y);
-                    xSemaphoreGive(dm542_USART3_Mutex);
-                    vTaskDelay(5000);
-                }
+                xSemaphoreTake(dm542_USART3_Mutex, portMAX_DELAY);
+                motor_position_ctrl(x, y);
+                currentMotorData.x = x;
+                currentMotorData.y = y;
+                xQueueSend(g_motorDataQueue, &currentMotorData, 10);
+                xSemaphoreGive(dm542_USART3_Mutex);
+                vTaskDelay(5000);
             }
-//						command->commandType = noDemand; // 命令完成
+          }
 
-            break;
+          break;
+        case demandMotorControl:
+          if (pdPASS == xQueueReceive(g_motorCmdQueue, &motorCmd, portMAX_DELAY))
+          {
+            motor_position_ctrl(motorCmd.x, motorCmd.y);
+            command->commandType = noDemand;
+          }
+
+          break;
+        case noDemand:
+          vTaskSuspend(NULL);
     }
 
-
+    vTaskDelay(10);
   }
 }
 
