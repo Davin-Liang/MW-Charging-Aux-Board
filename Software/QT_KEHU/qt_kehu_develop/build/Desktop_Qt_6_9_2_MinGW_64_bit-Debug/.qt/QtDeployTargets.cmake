7ac2ff4cb956488d357729d8f5294fa2b60e1abb
bcqlove <206263306@qq.com>
2026-02-02 09:51:38 +0800

    删除程序中间部分代码，精简分支

--- Software/QT_KEHU/qt_kehu_develop/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_PowerOptimizer_FILE C:/Users/bcq/Desktop/myplot/PowerOptimizer/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/PowerOptimizer.exe)
-set(__QT_DEPLOY_TARGET_PowerOptimizer_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PowerOptimizer_RUNTIME_DLLS D:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Network.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
