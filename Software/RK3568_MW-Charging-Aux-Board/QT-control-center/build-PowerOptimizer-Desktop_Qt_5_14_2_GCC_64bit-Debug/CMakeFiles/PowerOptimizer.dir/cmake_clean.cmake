6eb05910ca807c1d82e3c237ff43ae9bc829761c
bcqlove <20626306@qq.com>
2026-02-05 22:11:05 +0800

    cmake creat

---
+++ Software/RK3568_MW-Charging-Aux-Board/QT-control-center/build-PowerOptimizer-Desktop_Qt_5_14_2_GCC_64bit-Debug/CMakeFiles/PowerOptimizer.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "PowerOptimizer_autogen"
+  "CMakeFiles/PowerOptimizer_autogen.dir/AutogenOldSettings.cmake"
+  "ui_mainwindow.h"
+  "CMakeFiles/PowerOptimizer.dir/src/main.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/mainwindow.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/tab_netural.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/PID_Controller.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/TrajectoryJsonDialog.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/command_transmitter.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/tab_file_read.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/tab_stm32.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/tab_turntablecontrol.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/tab_deviceconnect.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/turntable_controller.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/src/logindialog.cpp.o"
+  "CMakeFiles/PowerOptimizer.dir/thirdparty/libmodbus/src/modbus.c.o"
+  "CMakeFiles/PowerOptimizer.dir/thirdparty/libmodbus/src/modbus-data.c.o"
+  "CMakeFiles/PowerOptimizer.dir/thirdparty/libmodbus/src/modbus-rtu.c.o"
+  "CMakeFiles/PowerOptimizer.dir/thirdparty/libmodbus/src/modbus-tcp.c.o"
+  "CMakeFiles/PowerOptimizer.dir/PowerOptimizer_autogen/mocs_compilation.cpp.o"
+  "PowerOptimizer.pdb"
+  "PowerOptimizer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/PowerOptimizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Software/RK3568_MW-Charging-Aux-Board/QT-control-center/build-PowerOptimizer-Desktop_Qt_5_14_2_GCC_64bit-Debug/CMakeFiles/PowerOptimizer_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "PowerOptimizer_autogen"
+  "CMakeFiles/PowerOptimizer_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/PowerOptimizer_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/PowerOptimizer_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
